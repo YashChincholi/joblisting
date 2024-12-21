0d53eeebdeaf423cc70b721aae5592e79a51065d
Your Name <you@example.com>
2024-12-21 12:20:39 +0530

    Create Serializers and Views and urls and migrations

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+class JobListCreate(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
